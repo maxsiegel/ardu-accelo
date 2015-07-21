bdeae13e252cbbaf223043e0b89d4a5f1f248e55
Max Siegel <maxs@mit.edu>
2015-07-21 13:56:58 -0400

    change receipt format

--- final/receive.py
+++ final/receive.py
@@ -8,20 +8,23 @@ from os.path import join
 timestamp = datetime.datetime.now().isoformat().replace('-', '_').replace(':', '_').replace('.', '_')
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-# s.settimeout(2)
 s.bind(('', 7400))
 
 t = 1;
 
-with open(join('/home/maxs/temp/ardu-test-files/', timestamp), 'wb'):
+filename = join('/home/maxs/temp/ardu-test-files/', timestamp)
+
+with open(filename, 'wt') as f:
+
     while True:
         data = s.recvfrom(1024)
-        # d.append(data)
-        if data[0] != '\n':
-            if data[0] != '\r':
-                print data[0]
-        # print(data.decode('utf-8'))
+
+        # print data
+
+        f.write(str(data[0]) + '\n')
+
         t += 1
         time.sleep(.01)
-        # print 'time: ' + str(t)
+        if not t % 10:
+            print('--------------------------------------------------------------------------------')
+            print('receiving data...' + str(t))
